16e6b745204d75d72540ccc535679c311933e4bd
DESKTOP-6IJ1U2T\shyoon <coodax@naver.com>
2025-03-05 08:59:42 +0900

    미리보기 설명 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: `wedding-Invitation`,
-    siteUrl: `https://seoklipse.github.io/wedding-invitation`
+    siteUrl: `https://seoklipse.github.io/wedding-invitation`,
+    description: `윤석현❤오수연 결혼식에 초대합니다`,
+    image: `https://seoklipse.github.io/wedding-invitation/KakaoThumbnail.png`,
   },
   pathPrefix: "/wedding-invitation",
   plugins: ["gatsby-plugin-styled-components"]
