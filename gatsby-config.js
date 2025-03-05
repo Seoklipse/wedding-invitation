fc0300d7fa5823bb6bd6a21ec7bda805a02f38a2
Seoklipse <coodax@naver.com>
2025-03-05 11:31:37 +0900

    카카오톡 공유하기 썸네일 이미지명 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `wedding-Invitation`,
     siteUrl: `https://seoklipse.github.io/wedding-invitation`,
     description: `윤석현❤오수연 결혼식에 초대합니다`,
-    image: `https://seoklipse.github.io/wedding-invitation/KakaoThumbnail.png`,
+    image: `https://seoklipse.github.io/wedding-invitation/KakaoThumbnail2.png`,
   },
   pathPrefix: "/wedding-invitation",
   plugins: ["gatsby-plugin-styled-components"]
